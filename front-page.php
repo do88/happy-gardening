05e1e7e8b0391d2792e7e42a23bd50c5ec5f8017
do88 <dmitry@dmitry-o.co.uk>
2021-03-02 18:35:22 +0000

    Refactor homepage

--- front-page.php
+++ front-page.php
@@ -1,17 +1,13 @@
-<?php get_header();?>
+<?php get_header();
+// Array of posts that appear on the homepage to exclude from latest posts below
+$home_posts_exclude = []; ?>
 
 <main id="primary" class="content">
-	<?php
-	// Array of posts that appear on the homepage to exclude from latest posts below
-	$home_page_posts = [];
-	?>
 
 	<?php /* ================================= Top Banner Image */ ?>
 
-	<?php
-	$featured_post = get_field('main_featured_post');
-	array_push($home_page_posts, $featured_post->ID);
-	?>
+	<?php $featured_post = get_field('main_featured_post');
+	array_push($home_posts_exclude, $featured_post->ID);  // add post to exclude array  ?>
 	<div class="home-banner">
 		<h2 class="home-banner__title">
 			<a href="<?php the_permalink($featured_post->ID) ?>">
@@ -28,27 +24,23 @@
 
 	<?php /* ================================= Secondary featured posts */ ?>
 
-	<?php
-	$secondary_featured_posts = get_field('secondary_featured_posts');
-	if( $secondary_featured_posts ) :
-	?>
+	<?php $secondary_featured_posts = get_field('secondary_featured_posts');
+	if( $secondary_featured_posts ) : ?>
 	<div class="featured-posts">
-		<?php foreach( $secondary_featured_posts as $post ):  ?>
-		<?php setup_postdata($post); ?>
-		<?php array_push($home_page_posts, $post->ID); ?>
-		<?php get_template_part( 'templates/parts/post-card'); ?>
-		<?php endforeach; ?>
+		<?php foreach( $secondary_featured_posts as $post ):
+			setup_postdata($post);
+			array_push($home_posts_exclude, $post->ID);  // add post to exclude array
+			get_template_part( 'templates/parts/post-card');
+		endforeach; ?>
 	</div>
 	<?php wp_reset_postdata(); ?>
 	<?php endif; ?>
 
 	<?php /* ================================= Banner post */ ?>
 
-	<?php
-	$banner_post = get_field('homepage_banner_post');
+	<?php $banner_post = get_field('homepage_banner_post');
 	if( $banner_post ) :
-	array_push($home_page_posts, $banner_post->ID);
-	?>
+	array_push($home_posts_exclude, $banner_post->ID); // add post to exclude array ?>
 	<a href="<?php the_permalink($banner_post->ID) ?>" class="banner-post">
 		<?php echo get_the_category_list(); ?>
 		<h2 class="banner-post__title"><?php echo get_the_title($banner_post->ID) ?></h2>
@@ -59,19 +51,17 @@
 	<?php /* ================================= Latest posts */ ?>
 
 	<div class="latest-posts">
-		<?php
-		$args = array(
+		<?php $args = array(
 			'post_type' => 'post',
 			'posts_per_page' => 3,
-			'post__not_in' => $home_page_posts,
+			'post__not_in' => $home_posts_exclude,
 		);
 		$latest_posts = new WP_Query($args);
-		?>
-		<?php while( $latest_posts->have_posts() ) : ?>
-		<?php $latest_posts->the_post(); ?>
-		<?php get_template_part( 'templates/parts/post-card'); ?>
-		<?php endwhile; ?>
-		<?php wp_reset_postdata(); ?>
+		while ($latest_posts->have_posts()):
+			$latest_posts->the_post();
+			get_template_part('templates/parts/post-card');
+		endwhile;
+		wp_reset_postdata(); ?>
 	</div>
 
 </main>
