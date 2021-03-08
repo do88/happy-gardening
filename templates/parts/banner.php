7b7ba409751e1acf8a913b7eef57cda413520c75
do88 <dmitry@dmitry-o.co.uk>
2021-03-08 22:04:29 +0000

    Single page development

--- templates/parts/banner.php
+++ templates/parts/banner.php
@@ -1,21 +1,30 @@
 <section class="banner">
 	<div class="banner__title">
+		<?php echo get_the_category_list(); ?>
 		<h1>
-			<?php
-			if (is_home()) {
-				echo single_post_title();
-			} elseif (is_404()) {
-				echo '404';
+			<?php if (is_404()) {
+				echo 'Page Not Found';
 			} else {
 				the_title();
 			} ?>
 		</h1>
+		<?php if (get_field('subtitle')) : ?>
+		<p class="banner__subtitle">
+			<?php the_field('subtitle') ?>
+		</p>
+		<?php endif; ?>
+		<div class="banner__date">
+			Posted: <time datetime="<?php echo get_the_date('c'); ?>" itemprop="datePublished"><?php echo get_the_date('F jS'); ?></time>
+		</div>
+		<div class="banner__readtime">
+			<?php echo do_shortcode('[rt_reading_time label="Read time:" postfix="minutes"]'); ?>
+		</div>
 	</div>
 	<div class="banner__image">
-		<?php if (!get_the_post_thumbnail()) : ?>
-			<img src="<?php echo get_template_directory_uri()."/dist/images/banner-home.jpg"?>" alt="">
-		<?php else :  ?>
-			<?php the_post_thumbnail('banner') ?>
-		<?php endif ?>
+		<?php if (!get_the_post_thumbnail()): ?>
+		<img src="<?php echo get_template_directory_uri() . "/dist/images/banner-home.jpg" ?>" alt="">
+		<?php else: ?>
+		<?php the_post_thumbnail('banner-image')?>
+		<?php endif?>
 	</div>
 </section>
